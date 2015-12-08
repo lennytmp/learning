796663d68359a41d8eb99af406b5ad012eb65fa2
Andrey Levin <lennytmp@gmail.com>
2015-12-08 23:58:12 +0000

    heapify & remove methods added

--- heap/Heap.java
+++ heap/Heap.java
@@ -4,12 +4,17 @@ import java.util.Arrays;
 
 class Node {
   public int value;
+  public int index;
+  
+  public int level;
+  
   public Node[] children = new Node[2];
   public Node parent;
   
 
-  public Node(int value) {
+  public Node(int value, int index) {
     this.value = value;
+    this.index = index;
   }
 
 
@@ -27,11 +32,44 @@ class Node {
       if (children[i] == null) {
         children[i] = child;
         child.parent = this;
+        child.level = this.level + 1;
         return;
       }
     }
     throw new RuntimeException("Trying to add child to full node");
   }
+
+
+  public void removeChild(Node child) {
+    for (int i = 0; i < children.length; i++) {
+      if (children[i] == child) {
+        child.parent = null;
+        children[i] = null;
+      }
+    }
+  }
+
+
+  public ArrayList<Node> getChildren() {
+    ArrayList<Node> result = new ArrayList();
+    for (Node ch : children) {
+      if (ch != null) {
+        result.add(ch);
+      }
+    }
+    return result; 
+  }
+
+  public Node getMaxChild() {
+    Node result = children[0];
+    for (Node ch : children) {
+      if (ch != null && ch.value > result.value) {
+        result = ch;
+      }
+    }
+    return result; 
+  }
+
 
 
   @Override
@@ -48,60 +86,134 @@ public class Heap {
   public void add(Node element) {
     if (root == null) {
       root = element;
+      root.level = 0;
     } else {
-      Node el = getFirstChildless(); 
+      Node el = getFirstFreeParent(); 
       el.addChild(element);
+      heapify(element);
     }
     this.elements.add(element);
   }
+
+
+  public Node findByIndex(int index) {
+    if (root == null) {
+      return null;
+    }
+    ArrayList<Node> queue = new ArrayList();
+    queue.add(root);
+    while (queue.size() > 0) {
+      Node node = queue.remove(0);
+      if (node.index == index) {
+        return node;
+      }
+      queue.addAll(node.getChildren());
+    }
+    return null;
+  }
+
+
+  public void remove(int index) {
+    Node elem = findByIndex(index);
+    if (elem == null) return;
+    Node last = getLastElement();
+    if (elem == last) {
+      elem.parent.removeChild(elem);
+      return;
+    }
+    Node lastParent = last.parent; 
+    if (elem.parent != null) {
+      Node p = elem.parent;
+      p.removeChild(elem);
+      p.addChild(last);
+    } else {
+      root = last;
+    }
+    for (int i = 0; i < elem.children.length; i++) {
+      if (elem.children[i] != null) {
+        elem.children[i].parent = last;
+      }
+    }
+    Node[] children = elem.children;
+    last.children = children;
+    last.level = elem.level;
+    lastParent.removeChild(last);
+   
+    ArrayList<Node> queue = new ArrayList();
+    queue.add(last);
+    while (queue.size() > 0) {
+      Node node = queue.remove(0);
+      Node maxChild = node.getMaxChild();
+      if (maxChild != null) {
+        if (maxChild.value > node.value) {
+          int tmp = maxChild.parent.value;
+          maxChild.parent.value = maxChild.value;
+          maxChild.value = tmp;
+          tmp = maxChild.parent.index;
+          maxChild.parent.index = maxChild.index;
+          maxChild.index = tmp;
+          queue.add(maxChild);
+        }
+      }
+    } 
+  }
+
+
+  public void heapify(Node elem) {
+    if (elem.parent != null && elem.parent.value < elem.value) {
+      int tmp = elem.parent.value;
+      elem.parent.value = elem.value;
+      elem.value = tmp;
+      tmp = elem.parent.index;
+      elem.parent.index = elem.index;
+      elem.index = tmp;
+      heapify(elem.parent);
+    }
+  }
+
 
   @Override
   public String toString() {
     String result = "";
     int currentLevel = 0;
-
-    class ExtNode {
-      public int level;
-      public Node node;
-
-      public ExtNode(Node node, int level) {
-        this.level = level;
-        this.node = node;
-      }
-    }
-
-    ArrayList<ExtNode> queue = new ArrayList();
-    queue.add(new ExtNode(root, 0));
+    ArrayList<Node> queue = new ArrayList();
+    queue.add(root);
     while (queue.size() > 0) {
-      ExtNode extNode = queue.remove(0);
-      if (currentLevel < extNode.level) {
+      Node node = queue.remove(0);
+      if (currentLevel < node.level) {
         result += "\n";
       }
-      currentLevel = extNode.level;
-      Node element = extNode.node;
-      if (element == null) {
-        continue;
-      }
-      result += element.toString() + "   ";
-      for (Node ch : element.children) {
-        if (ch != null) {
-          queue.add(new ExtNode(ch, extNode.level + 1));
-        }
-      }
+      currentLevel = node.level;
+      result += node.toString() + "   ";
+      queue.addAll(node.getChildren());
     }
     return result;
   }
 
 
-  public Node getFirstChildless() {
+  protected Node getLastElement() {
     ArrayList<Node> queue = new ArrayList();
+    if (root == null) return null;
+    queue.add(root);
+    Node element = null;
+    while (queue.size() > 0) {
+      element = queue.remove(0);
+      queue.addAll(element.getChildren());
+    }
+    return element;
+  }
+
+
+  protected Node getFirstFreeParent() {
+    ArrayList<Node> queue = new ArrayList();
+    if (root == null) return null;
     queue.add(root);
     while (queue.size() > 0) {
       Node element = queue.remove(0);
       if (element.getChildrenNum() < 2) {
         return element;
       }
-      queue.addAll(Arrays.asList(element.children));
+      queue.addAll(element.getChildren());
     }
     throw new RuntimeException("No childless element found");
   }
@@ -110,8 +222,10 @@ public class Heap {
   public static void main(String[] args) {
     Heap tree = new Heap();
     for (int i = 0; i < 10; i++) {
-      tree.add(new Node(i));
+      tree.add(new Node(i, i));
     }
+    System.out.println(tree);
+    tree.remove(3);
     System.out.println(tree);
   }
 }
