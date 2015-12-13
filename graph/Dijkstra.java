475724af15b589f9fd6b624cd310953eaf225da9
Levin-Andrey <lennytmp@gmail.com>
2015-12-13 11:43:44 +0000

    Dijkstra algorithm to calculate distances

--- graph/Dijkstra.java
+++ graph/Dijkstra.java
@@ -1,31 +1,57 @@
+/**
+ * Implements Dijkstran algorithm.
+ */
+
 package graph;
 
 import java.util.ArrayList;
 import heap.Heap;
+import java.util.HashMap;
 
 
 public class Dijkstra {
   /**
+   * Sets the adjList of the graph and calls getDistances function. 
    * @param args Array of arguments for launching the program. Ignored.
    */
   public static void main(String[] args) {
     int[][] adjList = {
       {1, 1, 2}, {1, 1, 4}, {1, 5, 3},
       {2, 1, 5},
-      {3, 2, 5}, {3, 3, 6}
+      {3, 3, 6},
+      {5, 2, 3}
     };
     Graph graph = new Graph(adjList);
     getDistances(graph, 1);
   }
 
-
+  /**
+   * Finds the distance of each node from the starting node with Dijkstra algorithm.
+   * @param graph The graph object.
+   * @param from The index of the node to calculate distance from.
+   */
   public static void getDistances(Graph graph, int from) {
     Heap edgesQueue = new Heap(false);
-    ArrayList<Edge> fromEdges = graph.nodesByIndex.get(from).edges;
-    for (Edge e : fromEdges) {
+    int currentCost = 0;
+    HashMap<Integer, Integer> distanceByIndex = new HashMap<Integer, Integer>();
+    distanceByIndex.put(from, 0);
+    ArrayList<Edge> newEdges = graph.nodesByIndex.get(from).edges;
+    for (Edge e : newEdges) {
       edgesQueue.addObj(e.cost, e);
     }
     Edge next = (Edge)edgesQueue.pullTopObj();
-    System.out.println(next.to.index);
+
+    while (next != null) {
+      Node nextNode = next.to;
+      if (distanceByIndex.get(nextNode.index) == null) {
+        distanceByIndex.put(nextNode.index, distanceByIndex.get(next.from.index) + next.cost);
+        newEdges = graph.nodesByIndex.get(nextNode.index).edges;
+        for (Edge e : newEdges) {
+          edgesQueue.addObj(e.cost, e);
+        }
+        System.out.println(next.to.index + " takes " + distanceByIndex.get(next.to.index));
+      }
+      next = (Edge)edgesQueue.pullTopObj();
+    }
   }
 }
