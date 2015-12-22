1f9fbc00c9d7610531cc19183bcedc28d65486f6
Levin-Andrey <lennytmp@gmail.com>
2015-12-22 23:01:58 +0000

    removing elements from search tree implemented

--- searchtree/Node.java
+++ searchtree/Node.java
@@ -18,6 +18,7 @@ class Node {
   //Value of the node.
   public int value;
 
+
   /**
    * Node constructor, sets the value.
    * @param value Integer value of the node.
@@ -25,6 +26,68 @@ class Node {
   public Node(int value) {
     this.value = value;
   }
+
+
+  /**
+   * @return Number of children which are not null.
+   */ 
+  public int childNum() {
+    if (leftChild != null && rightChild != null) {
+      return 2;
+    }
+    if (leftChild != null || rightChild != null) {
+      return 1;
+    }
+    return 0;
+  }
+
+
+  /**
+   * Removes child from element. 
+   * @param n Child node to remove.
+   */
+  public void removeChild(Node n) {
+    if (leftChild == n) {
+      leftChild = null;
+    }
+    if (rightChild == n) {
+      rightChild = null;
+    }
+  }
+
+
+  /**
+   * Gets the child node if there's only one,
+   * otherwise throws an exception.
+   * @return Child node.
+   */
+  public Node getTheOnlyChild() {
+    if (childNum() != 1) {
+      throw new RuntimeException("More than one child");
+    }
+    if (leftChild != null) {
+      return leftChild;
+    } else {
+      return rightChild;
+    }
+  }
+
+
+  /**
+   * Adds child to current node, updated child parent value.
+   * @param n Child node to add.
+   */
+  public void addChild(Node n) {
+    if (n.value < value && leftChild == null) {
+      leftChild = n;
+      leftChild.parent = this;
+    }
+    if (n.value >= value && rightChild == null) {
+      rightChild = n;
+      rightChild.parent = this;
+    }
+  }
+
 
   /**
    * Returns the node as a string including children's values.
--- searchtree/SearchTree.java
+++ searchtree/SearchTree.java
@@ -4,6 +4,7 @@
 
 package searchtree;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class SearchTree {
   //Root element.
@@ -11,7 +12,7 @@ public class SearchTree {
 
 
   /**
-   * Sets the tree.
+   * Sets the tree, calls functions and prints results.
    * @param args Array of arguments for launching the program. Ignored.
    */
   public static void main(String[] args) {
@@ -23,6 +24,23 @@ public class SearchTree {
     tree.add(21);
     tree.add(11);
     tree.add(1);
+    System.out.println(tree);
+    System.out.println("Removing 29");
+    tree.remove(29);
+    System.out.println(tree);
+    System.out.println("Removing 21");
+    tree.remove(21);
+    System.out.println(tree);
+    tree.add(12);
+    tree.add(13);
+    tree.add(3);
+    System.out.println("Adding 3 more elements");
+    System.out.println(tree);
+    System.out.println("Removing 10");
+    tree.remove(10);
+    System.out.println(tree);
+    System.out.println("Removing 2");
+    tree.remove(2);
     System.out.println(tree);
   }
 
@@ -43,20 +61,95 @@ public class SearchTree {
         if (curElem.leftChild != null) {
           curElem = curElem.leftChild;
         } else {
-          curElem.leftChild = n;
-          n.parent = curElem;
+          curElem.addChild(n);
           break;
         }
       } else {
         if (curElem.rightChild != null) {
           curElem = curElem.rightChild;
         } else {
-          curElem.rightChild = n;
-          n.parent = curElem;
+          curElem.addChild(n);
           break;
         }
       }
     }
+  }
+
+  /**
+   * Removes element by value. 
+   * @param value Value of the node to remove. 
+   */
+  public void remove(int value) {
+    Node n = findByValue(value);
+    if (n == null) {
+      return;
+    }
+    if (n.childNum() == 0) {
+      if (n == root) {
+        root = null;
+      } else {
+        n.parent.removeChild(n);
+      }
+      return;
+    }
+    if (n.childNum() == 1) {
+      if (n == root) {
+        root = n.getTheOnlyChild();
+      } else {
+        n.parent.removeChild(n);
+        n.parent.addChild(n.getTheOnlyChild());
+      }
+      return;
+    }
+    Node localMax = getMaxFrom(n);
+    remove(localMax.value);
+    n.value = localMax.value;
+    Node curElem = n;
+    while (true) {
+      if (curElem.rightChild == null ||
+          curElem.value < curElem.rightChild.value) {
+        return;
+      }
+      int tmp = curElem.rightChild.value;
+      curElem.rightChild.value = curElem.value;
+      curElem.value = tmp;
+      curElem = curElem.rightChild;
+    }
+  }
+
+
+  /**
+   * Gets the maximum starting from the node.
+   * @param n Node to start with.
+   * @return Node with the maximum value.
+   */
+  Node getMaxFrom(Node n) {
+    Node curElem = n;
+    while (curElem.rightChild != null) {
+      curElem = curElem.rightChild;
+    }
+    return curElem;
+  }
+
+
+  /**
+   * Finds the node in a tree by the value provided.
+   * @param value Value to search for. 
+   * @return Node or null if not found.
+   */
+  public Node findByValue(int value) {
+    Node curElem = root;
+    while (curElem != null) {
+      if (curElem.value == value) {
+        return curElem;
+      }
+      if (curElem.value > value) {
+        curElem = curElem.leftChild;
+      } else {
+        curElem = curElem.rightChild;
+      }
+    }
+    return null;
   }
 
   
@@ -67,6 +160,9 @@ public class SearchTree {
   @Override
   public String toString() {
     String result = "";
+    if (root == null) {
+      return "";
+    }
     ArrayList<Node> queue = new ArrayList<Node>();
     queue.add(root);
     while (queue.size() > 0) {
